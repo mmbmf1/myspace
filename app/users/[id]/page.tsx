e01cd1f17fe0ef81b4cf5c4686bef89b85529629
Michael Mace <michael.mace@conexon.us>
2023-05-22 12:34:25 -0500

    dynamic data

---
+++ app/users/[id]/page.tsx
@@ -0,0 +1,26 @@
+import { prisma } from '@/lib/prisma'
+import { Metadata } from 'next'
+
+interface Props {
+  params: {
+    id: string
+  }
+}
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const user = await prisma.user.findUnique({ where: { id: params.id } })
+  return { title: `User profile of ${user?.name}` }
+}
+
+export default async function UserProfile({ params }: Props) {
+  const user = await prisma.user.findUnique({ where: { id: params.id } })
+  const { name, bio, image } = user ?? {}
+
+  return (
+    <div className="mt-10">
+      <h1>{name}</h1>
+      <img src={image ?? '/mememan/webp'} alt={`${name}'s profile`} />
+      <h3 className="mt-2">Bio</h3>
+      <p>{bio ?? 'Unknown'}</p>
+    </div>
+  )
+}
